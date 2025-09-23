968fd18afb60285cd349fdc2ec1f959e546fd7d5
keerthnA <keerthanachandrank@Keerthanas-MacBook-Air.local>
2025-09-23 17:36:05 +0530

    fix : Create Customer when create new Member

--- gym_management/gym_management/doctype/member/member.js
+++ gym_management/gym_management/doctype/member/member.js
@@ -2,33 +2,6 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Member', {
-    setup(frm) {
-      frm.set_query("address", function (doc) {
-        return {
-          filters: {
-            link_doctype: "Member",
-            link_name: doc.name,
-          },
-        };
-      });
-    },
-    address(frm) {
-      if (frm.doc.address) {
-        frappe.call({
-          method: "frappe.contacts.doctype.address.address.get_address_display",
-          args: {
-            address_dict: frm.doc.address,
-          },
-          callback: function (r) {
-            let clean_address = r.message.replace(/<br\s*\/?>/gi, '\n');
-            frm.set_value("primary_address", r.message);
-          },
-        });
-      }
-      if (!frm.doc.address) {
-        frm.set_value("primary_address", "");
-      }
-    },
     dob(frm) {
         if (frm.doc.dob) {
             let dob = new Date(frm.doc.dob);
--- gym_management/gym_management/doctype/member/member.py
+++ gym_management/gym_management/doctype/member/member.py
@@ -1,9 +1,19 @@
 # Copyright (c) 2025, Keerthana and contributors
 # For license information, please see license.txt
-
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class Member(Document):
-	pass
+	def validate(self):
+		self.create_customer_against_member()
+
+	def create_customer_against_member(self):
+		"""Method to Create Customer Against Member """
+		if frappe.db.exists("Customer", self.name):
+			frappe.throw("{0} Already Exists".format(self.name))
+		customer = frappe.new_doc("Customer")
+		customer.customer_name = self.name
+		customer.customer_type = "Individual"
+		customer.gender = self.gender
+		customer.save()
